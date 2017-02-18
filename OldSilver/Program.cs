d30479a06c9ce35bfea0278198be189f4447ee44
Stringfellow <212567841@logon.ds.ge.com>
2017-02-18 15:57:51 -0800

    Add OldSilver project

---
+++ OldSilver/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OldSilver
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"i = {i}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
